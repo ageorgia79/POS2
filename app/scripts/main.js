9b1947bd2889981617ce97da1b5877cc6bab28b1
Andy Georgia <andygeorgia@hotmail.com>
2014-06-20 15:47:26 -0400

    now built on Parse

---
+++ app/scripts/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+Parse.initialize("LFJ3QGbWShkk4EL9OPC9RBzsM1beJbuSzZ3m8F0f", "h9DNGD2Piozeww9Il1F9zS85gQh17uJXZnqf2l7V");
+
+var Login = Parse.Object.extend({
+  className: "Login"
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+var LoginCollection = Parse.Collection.extend({
+
+  model: Login,
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+var collection = new LoginCollection();
+
+
+var LoginView = Parse.View.extend({
+
+  className: "cool",
+
+  loginTemplate: _.template($('.login-template').text()),
+
+  events: {
+
+    //"click .submit": "showLayoutView"
+  },
+
+  initialize: function(){
+    $('.container').append(this.el);
+    this.render();
+  },
+
+  render: function(){
+    var renderedTemplate = this.loginTemplate();
+    this.$el.html(renderedTemplate);
+  }
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+var view = new LoginView();
+
+
+
